8139d21268b74375e4e7f82e2335b14c0264b4a1
Yaorui Xu <yaoruixu@Yaoruis-MacBook-Air.local>
2025-06-09 17:37:15 -0400

    added scipt to to pdf files to gcs bucket

---
+++ cloud_storage.py
@@ -0,0 +1,22 @@
+from google.cloud import storage
+
+def upload_blob(bucket_name,  source_file_name, destination_blob_name):
+    '''Uploads a local file to the bucket'''
+
+    # create client
+    storage_client = storage.Client()
+    # get bucket
+    bucket = storage_client.bucket(bucket_name)
+    # get blob
+    blob = bucket.blob(destination_blob_name)
+
+    #  generation-match precondition
+    generation_match_precondition = 0
+
+    # upload to blob
+    blob.upload_from_filename(source_file_name, if_generation_match = generation_match_precondition)
+
+    print(f"File {source_file_name} uploaded to {bucket_name}")
+
+
+upload_blob("ocr-pdf-bucket-68", "/Users/yaoruixu/Downloads/einstein_general_relativity.pdf", "einstein paper")
